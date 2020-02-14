8c96049b10c8bf8e5141d9e3f481ebe911f81b41
Jairo <jairobernaldev@gmail.com>
2020-02-14 12:12:45 +0100

    Ejemplo terminado.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from graphene import ObjectType, String, Schema
 class Query(ObjectType):
     hello = String(name=String(default_value="stranger"))
-    googbay = String()
+    goodbye = String()
 
     def resolve_hello(root, info, name):
         return f'Hello {name}!'
@@ -10,3 +10,15 @@ class Query(ObjectType):
         return 'See ya'
 
 schema = Schema(query=Query)
+
+query_string = '{hello}'
+result = schema.execute(query_string)
+print(result.data['hello'])
+
+query_with_argument = '{hello(name:"Jairo")}'
+result = schema.execute(query_with_argument)
+print(result.data['hello'])
+
+query_goodbye = '{goodbye}'
+result = schema.execute(query_goodbye)
+print(result.data['goodbye'])
